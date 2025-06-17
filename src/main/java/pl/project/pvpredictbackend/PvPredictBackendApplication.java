2330e4341df644d07e521e2fca0feece94f160b4
przemak19 <przemak19@gmail.com>
2025-06-17 22:28:43 +0200

    Initial commit

---
+++ src/main/java/pl/project/pvpredictbackend/PvPredictBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.project.pvpredictbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvPredictBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PvPredictBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/project/pvpredictbackend/PvPredictBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.project.pvpredictbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PvPredictBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
