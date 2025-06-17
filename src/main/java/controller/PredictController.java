64f7965dde74b06a7b19173183c83ddfeab5a5f5
przemak19 <przemak19@gmail.com>
2025-06-18 00:55:42 +0200

    Add PredictController with both endpoints

--- src/main/java/controller/PredictController.java
+++ src/main/java/controller/PredictController.java
@@ -1,8 +1,14 @@
 package controller;
 
+import dto.DailyInformationDto;
+import dto.WeeklySummaryDto;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import service.PredictService;
+
+import java.util.List;
 
 /**
  * REST controller exposing endpoints to retrieve solar forecast and weather summaries for a given location.
@@ -17,5 +23,27 @@ public class PredictController {
         this.predictService = predictService;
     }
 
+    /**
+     * Endpoint to seven days forecast
+     * @param latitude user geographic latitude
+     * @param longitude user geographic longitude
+     */
+    @GetMapping("/forecast")
+    public List<DailyInformationDto> get7DayForecast(
+            @RequestParam double latitude,
+            @RequestParam double longitude) {
+        return predictService.getWeekForecast(latitude, longitude);
+    }
 
+    /**
+     * Summary of the week
+     * @param latitude user geographic latitude
+     * @param longitude user geographic longitude
+     */
+    @GetMapping("/summary")
+    public WeeklySummaryDto getWeeklySummary(
+            @RequestParam double latitude,
+            @RequestParam double longitude) {
+        return predictService.getSummary(latitude, longitude);
+    }
 }
