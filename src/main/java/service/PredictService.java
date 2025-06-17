18160e2f1042bbc0993115aa5f4f1bd1a6171d6f
przemak19 <przemak19@gmail.com>
2025-06-18 00:48:04 +0200

    Add second endpoint of PredictService

--- src/main/java/service/PredictService.java
+++ src/main/java/service/PredictService.java
@@ -68,6 +68,51 @@ public class PredictService {
      */
     public WeeklySummaryDto getSummary(double latitude, double longitude) {
 
-        return null;
+        OpenMeteoResponseDto response = openMeteoClient.getWeeklyForecast(latitude, longitude);
+
+        List<Double> pressure = response.getDaily().getPressure_msl();
+        List<Double> sunshine = response.getDaily().getSunshine();
+        List<Double> minTemps = response.getDaily().getTemperature_2m_min();
+        List<Double> maxTemps = response.getDaily().getTemperature_2m_max();
+        List<Integer> weatherCodes = response.getDaily().getWeather_code();
+
+        // calculate average pressure
+        double avgPressure = pressure.stream().mapToDouble(Double::doubleValue).average().orElse(0);
+        // calculate average sunshine
+        double avgSunshineHours = sunshine.stream().mapToDouble(s -> s / 3600.0).average().orElse(0);
+
+        // find minimum temperature
+        double minTemperature = minTemps.stream().mapToDouble(Double::doubleValue).min().orElse(0);
+        // find maximum temperature
+        double maxTemperature = maxTemps.stream().mapToDouble(Double::doubleValue).max().orElse(0);
+
+        // count days with precipitation using method isRainyWeather
+        long precipitationDaysCount = weatherCodes.stream()
+                .filter(this::isPrecipitation)
+                .count();
+
+        String summary = (precipitationDaysCount >= 4) ? "z opadami" : "bez opadów";
+
+        WeeklySummaryDto summaryDto = new WeeklySummaryDto();
+
+        summaryDto.setAverageWeekPressure(avgPressure);
+        summaryDto.setAverageWeekSunExposureHours(avgSunshineHours);
+        summaryDto.setMinWeekTemperature(minTemperature);
+        summaryDto.setMaxWeekTemperature(maxTemperature);
+        summaryDto.setGeneralWeatherSummary(summary);
+
+        return summaryDto;
+    }
+
+    /**
+     * Simple method to determine if given weather code is precipitation.
+     * (Based on Open-Meteo docs) Codes 51-67, 80-82 is rain, 71-77 is snow, 85-86 is snowfall.
+     */
+    private boolean isPrecipitation(int weatherCode) {
+
+        return (weatherCode >= 51 && weatherCode <= 67) ||
+                (weatherCode >= 80 && weatherCode <= 82) ||
+                (weatherCode >= 71 && weatherCode <= 77) ||
+                (weatherCode >= 85 && weatherCode <= 86);
     }
 }
