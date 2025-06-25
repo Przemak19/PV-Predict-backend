8555949ec48738114f93f3bbded5f3e782c87057
Przemak19 <przemak19@gmail.com>
2025-06-25 23:12:32 +0200

    Fix Validation for params in forecast method in controller

--- src/main/java/pl/project/pvpredictbackend/controller/PredictController.java
+++ src/main/java/pl/project/pvpredictbackend/controller/PredictController.java
@@ -35,7 +35,7 @@ public class PredictController {
     @GetMapping("/forecast")
     public List<DailyInformationDto> get7DayForecast(
             @RequestParam @DecimalMin("-90.0") @DecimalMax("90.0") double latitude,
-            @RequestParam double longitude) {
+            @RequestParam @DecimalMin("-180.0") @DecimalMax("180.0") double longitude) {
         return predictService.getWeekForecast(latitude, longitude);
     }
 
